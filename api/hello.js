94b17f8aea89b545ad0be97bd93348020ad366f7
SJ Hayman <sj@e2.ltd>
2023-08-13 20:58:46 +0100

    Example using env var

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,3 @@
 export default async function handler(request, response) {
-  return response.status(200).json({ hello: 'world' });
+  return response.status(200).json({ hello: `${process.env.FOO}` });
 }
